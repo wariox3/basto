388329bf63de6c46302942cdfbbf0ca7abd42a75
desarrollo <desarrollo@desarrollo-PC>
2014-02-05 15:26:42 -0500

    Implementacion inicial

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Basto\FrontEndBundle\BastoFrontEndBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
